51e88cd0c4eb2482b41bd5fcbf09bf7271ad2138
Madalina Buga <mada.buga@yahoo.com>
2021-11-06 20:08:03 +0200

    Added 'category' constant for icons

---
+++ client/src/constants/categories.js
@@ -0,0 +1,260 @@
+export const CATEGORY_ICONS = [
+    {
+        iconName: "faShoppingCart"
+    },
+    {
+        iconName: "faShoppingBag"
+    },
+    {
+        iconName: "faTshirt"
+    },
+    {
+        iconName: "faGifts"
+    },
+    {
+        iconName: "faGift"
+    },
+    {
+        iconName: "faGem"
+    },
+    {
+        iconName: "faFileInvoice"
+    },
+    {
+        iconName: "faFileInvoiceDollar"
+    },
+    {
+        iconName: "faMoneyBillWave"
+    },
+    {
+        iconName: "faFileInvoiceDollar"
+    },
+    {
+        iconName: "faWallet"
+    },
+    {
+        iconName: "faUmbrellaBeach"
+    },
+    {
+        iconName: "faPlaneDeparture"
+    },
+    {
+        iconName: "faCocktail"
+    },
+    {
+        iconName: "faUtensils"
+    },
+    {
+        iconName: "faHamburger"
+    },
+    {
+        iconName: "faShoePrints"
+    },
+    {
+        iconName: "faHospitalAlt"
+    },
+    {
+        iconName: "faMedkit"
+    },
+    {
+        iconName: "faTools"
+    },
+    {
+        iconName: "faHammer"
+    },
+    {
+        iconName: "faPaw"
+    },
+    {
+        iconName: "faDog"
+    },
+    {
+        iconName: "faCat"
+    },
+    {
+        iconName: "faHome"
+    },
+    {
+        iconName: "faCar"
+    },
+    {
+        iconName: "faUserFriend"
+    },
+    {
+        iconName: "faMobileAlt"
+    },
+    {
+        iconName: "faWineGlass"
+    },
+    {
+        iconName: "faCoffee"
+    },
+    {
+        iconName: "faBus"
+    },
+    {
+        iconName: "faSubway"
+    },
+    {
+        iconName: "faBicycle"
+    },
+    {
+        iconName: "faMotocycle"
+    },
+    {
+        iconName: "faRunning"
+    },
+    {
+        iconName: "faDumbbell"
+    },
+    {
+        iconName: "faSnowboarding"
+    },
+    {
+        iconName: "faSkiing"
+    },
+    {
+        iconName: "faSkating"
+    },
+    {
+        iconName: "faSwimmer"
+    },
+    {
+        iconName: "faFutbol"
+    },
+    {
+        iconName: "faBasketballBall"
+    },
+    {
+        iconName: "faBowlingBall"
+    },
+    {
+        iconName: "faFootballBall"
+    },
+    {
+        iconName: "faTableTennis"
+    },
+    {
+        iconName: "faBaseballBall"
+    },
+    {
+        iconName: "faVolleyBallBall"
+    },
+    {
+        iconName: "faGamepad"
+    },
+    {
+        iconName: "faDice"
+    },
+    {
+        iconName: "faTrophy"
+    },
+    {
+        iconName: "faPuzzlePiece"
+    },
+    {
+        iconName: "faHeadset"
+    },
+    {
+        iconName: "faPills"
+    },
+    {
+        iconName: "faTooth"
+    },
+    {
+        iconName: "faUserMd"
+    },
+    {
+        iconName: "faUserNurse"
+    },
+    {
+        iconName: "faBaby"
+    },
+    {
+        iconName: "faBabyCarriage"
+    },
+    {
+        iconName: "faBeer"
+    },
+    {
+        iconName: "faGlassMartiniAlt"
+    },
+    {
+        iconName: "faMugHot"
+    },
+    {
+        iconName: "faTree"
+    },
+    {
+        iconName: "faSeedling"
+    },
+    {
+        iconName: "faSpa"
+    },
+    {
+        iconName: "faTv"
+    },
+    {
+        iconName: "faCamera"
+    },
+    {
+        iconName: "faLaptop"
+    },
+    {
+        iconName: "faLaptopCode"
+    },
+    {
+        iconName: "faCreditCard"
+    },
+    {
+        iconName: "faBook"
+    },
+    {
+        iconName: "faBookOpen"
+    },
+    {
+        iconName: "faPalette"
+    },
+    {
+        iconName: "faHeart"
+    },
+    {
+        iconName: "faMusic"
+    },
+    {
+        iconName: "faMountain"
+    },
+    {
+        iconName: "faTram"
+    },
+    {
+        iconName: "faPen"
+    },
+    {
+        iconName: "faEnvelopeOpenText"
+    },
+    {
+        iconName: "faFolder"
+    },
+    {
+        iconName: "faIceCream"
+    },
+    {
+        iconName: "faBirthdayCake"
+    },
+    {
+        iconName: "faParking"
+    },
+    {
+        iconName: "faShip"
+    },
+    {
+        iconName: "faShapes"
+    },
+    {
+        iconName: "faStoreAlt"
+    },
+    {
+        iconName: "faPizzaSlice"
+    }
+]
